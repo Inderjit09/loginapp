00965065e8a85f6d99fb66811776165446b83ebe
Inderjit Singh <erinderbrar1@gmail.com>
2021-03-17 18:17:18 -0400

    Final

--- Screen1.js
+++ Screen1.js
@@ -2,15 +2,12 @@ import React, { Component } from 'react'
 import { View, Button, TextInput } from 'react-native'
 
 export default class Screen1 extends Component {
-
-    constructor(props)
-    {
+constructor(props) {
         super(props);
         this.state={
             username:''
         };
     }
-
     render() 
     {
         return 
@@ -21,13 +18,12 @@ export default class Screen1 extends Component {
                     style={{borderBottomWidth:1, borderBottomColor:'#ff0000', marginbottom:20 }}
                     onChangeText={username=>this.setState({username})}>
                 </TextInput>
-
                 <TextInput placeholder="Enter your Password"
                     style={{borderBottomWidth:1, borderBottomColor:'#ff0000', marginbottom:20 }}
                     onChangeText={username=>this.setState({username})}>
                 </TextInput>
                 
-                <Button title="Enter" 
+                <Button title="Submit" 
                     onPress={()=>this.props.navigation.navigate('Screen2', {Parameter1:this.state.username})}>
                 </Button>
 
